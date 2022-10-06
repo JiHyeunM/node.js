4bcbe9e504d0b041d465ef20348aed2c8ba9dce3
JiHyeunM <moon011011@gmail.com>
2022-10-06 17:45:06 +0900

    env

--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,20 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // const config = require("../config");
 
+// const connect = () => {
+// mongoose.connect("mongodb+srv://jihyeun_moon:wlgus5490@cluster0.1nmoovx.mongodb.net/?retryWrites=true&w=majority", { ignoreUndefined: true })
+//         .catch((err) => {
+// console.error(err);
+// });
+// };
+
+require("dotenv").config();
+const mongoose = require("mongoose");
 const connect = () => {
-mongoose.connect("mongodb+srv://jihyeun_moon:wlgus5490@cluster0.1nmoovx.mongodb.net/?retryWrites=true&w=majority", { ignoreUndefined: true })
-        .catch((err) => {
-console.error(err);
-});
-};
+        mongoose.connect(process.env.DB_URL, { ignoreUndefined: true }).catch((err) => {
+        console.error(err);
+        res.status(400)
+        });
+        };
 
 module.exports = connect;

8059852a7ddd282dfe3d4cf1f20997988911372b
JiHyeunM <moon011011@gmail.com>
2022-10-06 17:46:27 +0900

    찌이이이인

--- schemas/index.js
+++ schemas/index.js
@@ -1,13 +1,3 @@
-// const mongoose = require("mongoose");
-// const config = require("../config");
-
-// const connect = () => {
-// mongoose.connect("mongodb+srv://jihyeun_moon:wlgus5490@cluster0.1nmoovx.mongodb.net/?retryWrites=true&w=majority", { ignoreUndefined: true })
-//         .catch((err) => {
-// console.error(err);
-// });
-// };
-
 require("dotenv").config();
 const mongoose = require("mongoose");
 const connect = () => {
